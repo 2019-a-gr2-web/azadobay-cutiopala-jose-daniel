38536aa6cb7d2103cda5651003bc1f90e877ee94
DanyJos3 <jose196.jm@hotmail.com>
2019-04-10 16:01:22 -0500

    Codigo de Estado

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() //Metodo http
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post() //Metodo http post
+  @HttpCode(200)
+  postHello(){
+    return 'Hola mundo en post';
   }
 }
