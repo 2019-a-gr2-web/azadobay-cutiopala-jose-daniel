7ac4b0d623effb3f1eb8c631216fc689d91b62d3
DanyJos3 <jose196.jm@hotmail.com>
2019-04-30 14:52:28 -0500

    Optimizacion Calculadora-Cookies

--- 01-http/02-servidor-web-nodejs/calculadora/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/calculadora/src/app.controller.ts
@@ -10,35 +10,49 @@ export class AppController {
   getSuma(@Headers() headers, @Request() req, @Response() res) {
 
     if (headers.numero1 && headers.numero2) {
-
-      const cookieSegura = req.signedCookies;
       const puntos = req.signedCookies.Dany;
-
-      if (cookieSegura.Dany) {
-        const resultado = Number(headers.numero1) + Number(headers.numero2);
-        const puntosActuales = Number(puntos) - resultado;
-
-        if (puntosActuales < 0) {
-          res.send({
-            Mesaje: 'No se puede realizar la operación',
-          })
-        }
-        if (puntosActuales === 0) {
-
-          this.crearCookie(
-            resultado,
-            puntosActuales,
-            res,
-            'Se le acabaron los puntos')
-        } else {
-          this.crearCookie(resultado, puntosActuales, res, 'Operacion Realizada');
-        }
-      } else {
-        this.crearCookie(0, 100, res, 'Cookie Creada');
-      }
-
+      const resultado = Number(headers.numero1) + Number(headers.numero2);
+      this.realizarOperacion(puntos, resultado, req, res);
     } else { res.send(`No se puede realizar la suma.`) }
   }
+
+  @Post('/Restar')
+  @HttpCode(201)
+  postResta(@Body() body, @Request() req, @Response() res) {
+    if (body.numero1 && body.numero2) {
+      const puntos = req.signedCookies.Dany;
+      const resultado = Number(body.numero1) - Number(body.numero2);
+      this.realizarOperacion(puntos, resultado, req, res);
+    } else {
+      return `No se puede realizar la resta.`;
+    }
+  }
+
+  @Put('/Multiplicar')
+  @HttpCode(202)
+  putMultiplicacion(@Query() queryParams, @Request() req, @Response() res) {
+    if (queryParams.numero1 && queryParams.numero2) {
+      const puntos = req.signedCookies.Dany;
+      const resultado = Number(queryParams.numero1) * Number(queryParams.numero2);
+      this.realizarOperacion(puntos, resultado, req, res);
+    } else {
+      return `No se puede realizar la multiplicacion.`;
+    }
+
+  }
+
+  @Delete('/Dividir')
+  @HttpCode(203)
+  deleteDividir(@Query() queryParams, @Body() body, @Response() res, @Request() req) {
+    if (queryParams.numero1 && body.numero2) {
+      const puntos = req.signedCookies.Dany;
+      const resultado = Number(queryParams.numero1) / Number(body.numero2);
+      this.realizarOperacion(puntos, resultado, req, res);
+    } else {
+      return res.send({ mensaje: 'No se pudo realizar la division' });
+    }
+  }
+
 
   crearCookie(resultado, nuevoValor, @Response() res, mensaje) {
     res
@@ -54,113 +68,29 @@ export class AppController {
       });
   }
 
-  @Post('/Restar')
-  @HttpCode(201)
-  postResta(@Body() body, @Request() req, @Response() res) {
+  realizarOperacion(puntos, resultado, @Request() req, @Response() res) {
+    const cookieSegura = req.signedCookies;
+    if (cookieSegura.Dany) {
 
+      const puntosActuales = Number(puntos) - resultado;
 
-    if (body.numero1 && body.numero2) {
-      const cookieSegura = req.signedCookies;
-      const puntos = req.signedCookies.Dany;
-
-      if (cookieSegura.Dany) {
-        const resultado = Number(body.numero1) - Number(body.numero2);
-        const puntosActuales = Number(puntos) - resultado;
-
-        if (puntosActuales < 0) {
-          res.send({
-            Mesaje: 'No se puede realizar la operación',
-          })
-        }
-        if (puntosActuales === 0) {
-
-          this.crearCookie(
-            resultado,
-            puntosActuales,
-            res,
-            'Se le acabaron los puntos')
-        } else {
-          this.crearCookie(resultado, puntosActuales, res, 'Operacion Realizada');
-        }
-      } else {
-        this.crearCookie(0, 100, res, 'Cookie Creada');
+      if (puntosActuales < 0) {
+        res.send({
+          Mesaje: 'No se puede realizar la operación',
+        })
       }
+      if (puntosActuales === 0) {
 
-    } else {
-      return `No se puede realizar la resta.`;
-    }
-  }
-
-  @Put('/Multiplicar')
-  @HttpCode(202)
-  putMultiplicacion(@Query() queryParams, @Request() req, @Response() res) {
-
-    if (queryParams.numero1 && queryParams.numero2) {
-
-      const cookieSegura = req.signedCookies;
-      const puntos = req.signedCookies.Dany;
-
-      if (cookieSegura.Dany) {
-        const resultado = Number(queryParams.numero1) * Number(queryParams.numero2);
-        const puntosActuales = Number(puntos) - resultado;
-
-        if (puntosActuales < 0) {
-          res.send({
-            Mesaje: 'No se puede realizar la operación',
-          })
-        }
-        if (puntosActuales === 0) {
-
-          this.crearCookie(
-            resultado,
-            puntosActuales,
-            res,
-            'Se le acabaron los puntos')
-        } else {
-          this.crearCookie(resultado, puntosActuales, res, 'Operacion Realizada');
-        }
+        this.crearCookie(
+          resultado,
+          puntosActuales,
+          res,
+          'Se le acabaron los puntos')
       } else {
-        this.crearCookie(0, 100, res, 'Cookie Creada');
-      }
-
-    } else {
-      return `No se puede realizar la multiplicacion.`;
-    }
-
-  }
-
-  @Delete('/Dividir')
-  @HttpCode(203)
-  deleteDividir(@Query() queryParams, @Body() body, @Response() res, @Request() req) {
-
-    if (queryParams.numero1 && body.numero2) {
-      const cookieSegura = req.signedCookies;
-      const puntos = req.signedCookies.Dany;
-
-      if (cookieSegura.Dany) {
-        const resultado = Number(queryParams.numero1) / Number(body.numero2);
-        const puntosActuales = Number(puntos) - resultado;
-
-        if (puntosActuales < 0) {
-          res.send({
-            Mesaje: 'No se puede realizar la operación',
-          })
-        }
-        if (puntosActuales === 0) {
-
-          this.crearCookie(
-            resultado,
-            puntosActuales,
-            res,
-            'Se le acabaron los puntos')
-        } else {
-          this.crearCookie(resultado, puntosActuales, res, 'Operacion Realizada');
-        }
-      } else {
-        this.crearCookie(0, 100, res, 'Cookie Creada');
+        this.crearCookie(resultado, puntosActuales, res, 'Operacion Realizada');
       }
     } else {
-      return res.send({ mensaje: 'No se pudo realizar la division' });
+      this.crearCookie(0, 100, res, 'Cookie Creada');
     }
   }
 
