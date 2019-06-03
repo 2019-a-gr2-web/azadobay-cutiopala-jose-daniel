a79c07e902eeddfbe736006c5c36b4ccef90efa9
JennyCastro <34132068+JennyCastro@users.noreply.github.com>
2019-06-03 00:46:28 -0500

    pruebas

--- 01-http/03-exámen/entrenador-pokemon/src/app.module.ts
+++ 01-http/03-exámen/entrenador-pokemon/src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import {PokemonModule} from "./Pokemon/pokemon.module";
+import {LoginModule} from "./Login/login.module";
+import {EntrenadorModule} from "./Entrenador/entrenador.module";
+
+
 @Module({
-  imports: [],
+  imports: [PokemonModule, LoginModule, EntrenadorModule],
   controllers: [AppController],
   providers: [AppService],
 })
